5294d172e3e11c1149e61941558d3e920f979721
Juan Carlos <jcarlosjccm@gmail.com>
2023-10-27 10:11:33 +0200

    Preparado el html de registro

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistroComponent } from './pages/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    children: [
+      { path: 'register', component: RegistroComponent },
+      { path: '**', redirectTo: 'register'}
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 
 @NgModule({
   declarations: [
+    RegistroComponent
   ],
   imports: [
     CommonModule,
